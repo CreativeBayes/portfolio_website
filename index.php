da43affe11ca98ccdfee6ce6892c851e073c98f8
CreativeBayes <steevn.pyram@gmail.com>
2025-03-10 10:00:57 +0100

    correctif php

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="../public/css/style.css" />
+    <link rel="stylesheet" href="../public/css/components/footer.css">
+    <link rel="stylesheet" href="../public/css/components/header.css">
+    <link rel="stylesheet" href="../../my-website/views/templates/componants/cta.css">
+    <style>
+      @import url("https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&family=Roboto:ital,wght@0,100..900;1,100..900&family=Rosario:ital,wght@0,300..700;1,300..700&display=swap");
+    </style>
+    <title>steevn's portfolio</title>
+  </head>
 <?php
 include ('header.html');
 include ('index.html');
 include ('footer.html');
 ?>
+</html>
+
